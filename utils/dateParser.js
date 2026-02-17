8c94b4eb06ba47737431f6049b41337cbefa38e1
Jhonsy Rodallega <147451233+RodallegaJhonsy@users.noreply.github.com>
2026-02-16 23:33:49 -0500

    Add files via upload

--- utils/dateParser.js
+++ utils/dateParser.js
@@ -1,1 +1,11 @@
+const chrono = require('chrono-node');
 
+const parseDate = (text) => {
+    const results = chrono.es.parse(text);
+    if (results.length > 0) {
+        return results[0].start.date();
+    }
+    return null;
+};
+
+module.exports = { parseDate };
